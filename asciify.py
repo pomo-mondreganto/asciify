ee399aed9611f86219b212ca8027a3c1499d0fda
Roman Nikitin <nikrom2012@me.com>
2019-11-05 22:13:36 +0300

    Optimized for running by scripts

--- asciify.py
+++ asciify.py
@@ -1,91 +1,124 @@
 from PIL import Image
 
-ASCII_CHARS = ['.',',',':',';','+','*','?','%','S','#','@']
+import os
+import urllib
+import argparse
+
+ASCII_CHARS = ['.', ',', ':', ';', '+', '*', '?', '%', 'S', '#', '@']
 ASCII_CHARS = ASCII_CHARS[::-1]
 
-'''
-method resize():
-    - takes as parameters the image, and the final width
-    - resizes the image into the final width while maintaining aspect ratio
-'''
-def resize(image, new_width=100):
+
+def resize(image, new_width=100, ar_multiplier=1.0):
+    """
+    Resizes an image to match new width and multiplies aspect ratio
+    by ar_multiplier (to match line intervals)
+
+    :param ar_multiplier: a float to multiply aspect ratio with
+    :param image: image to convert
+    :param new_width: new width of the image in chars
+    :return:
+    """
     (old_width, old_height) = image.size
-    aspect_ratio = float(old_height)/float(old_width)
+    aspect_ratio = float(old_height) / float(old_width)
+    aspect_ratio *= ar_multiplier
+
     new_height = int(aspect_ratio * new_width)
     new_dim = (new_width, new_height)
     new_image = image.resize(new_dim)
+
     return new_image
-'''
-method grayscalify():
-    - takes an image as a parameter
-    - returns the grayscale version of image
-'''
+
+
 def grayscalify(image):
+    """
+    Converts image to grayscale
+
+    :param image: image to convert
+    :return: grayscale image
+    """
     return image.convert('L')
 
-'''
-method modify():
-    - replaces every pixel with a character whose intensity is similar
-'''
-def modify(image, buckets=25):
+
+def modify(image):
+    """
+    Converts image to ascii art
+
+    :param image: image to convert
+    :return: string of ascii art
+    """
+    bucket_size = 255 // len(ASCII_CHARS) + bool(255 % len(ASCII_CHARS))
     initial_pixels = list(image.getdata())
-    new_pixels = [ASCII_CHARS[pixel_value//buckets] for pixel_value in initial_pixels]
+    new_pixels = [ASCII_CHARS[pixel_value // bucket_size] for pixel_value in initial_pixels]
     return ''.join(new_pixels)
 
-'''
-method do():
-    - does all the work by calling all the above functions
-'''
+
 def do(image, new_width=100):
-    image = resize(image)
+    """
+    Converts to grayscale, then to ascii art
+
+    :param image: image to convert
+    :param new_width: new image width
+    :return: a text with ascii art
+    """
+    image = resize(image, new_width=new_width)
     image = grayscalify(image)
 
     pixels = modify(image)
     len_pixels = len(pixels)
 
-    # Construct the image from the character list
-    new_image = [pixels[index:index+new_width] for index in range(0, len_pixels, new_width)]
+    new_image = [pixels[index:index + new_width] for index in range(0, len_pixels, new_width)]
 
     return '\n'.join(new_image)
 
-'''
-method runner():
-    - takes as parameter the image path and runs the above code
-    - handles exceptions as well
-    - provides alternative output options
-'''
-def runner(path):
+
+def convert(image_path, result_path, out_width):
     image = None
     try:
-        image = Image.open(path)
-    except Exception:
-        print("Unable to find image in",path)
-        #print(e)
-        return
-    image = do(image)
+        image = Image.open(image_path)
+    except Exception as e:
+        print(f"Invalid image in path {image_path}, exception: {e}")
+        exit(1)
 
-    # To print on console
-    print(image)
+    image = do(image, new_width=out_width)
 
-    # Else, to write into a file
-    # Note: This text file will be created by default under
-    #       the same directory as this python file,
-    #       NOT in the directory from where the image is pulled.
-    f = open('img.txt','w')
-    f.write(image)
-    f.close()
+    print(result_path)
+    with open(result_path, 'w') as f:
+        f.write(image)
 
-'''
-method main():
-    - reads input from console
-    - profit
-'''
+    print('Done!')
+
+
 if __name__ == '__main__':
-    import sys
-    import urllib.request
-    if sys.argv[1].startswith('http://') or sys.argv[1].startswith('https://'):
-        urllib.request.urlretrieve(sys.argv[1], "asciify.jpg")
-        path = "asciify.jpg"
+    parser = argparse.ArgumentParser(description='Convert image to ASCII art')
+    parser.add_argument(
+        '-i', '--infile',
+        type=str, metavar='PATH',
+        help='Image to convert (path or url)',
+        required=True,
+    )
+    parser.add_argument(
+        '-o', '--outfile',
+        type=str, metavar='PATH',
+        help='File to write result in',
+        required=True,
+    )
+    parser.add_argument(
+        '-w', '--width',
+        type=int, metavar='N',
+        help='Width of output in chars',
+        default=100,
+    )
+    args = parser.parse_args()
+
+    infile = args.infile
+    outfile = args.outfile
+    width = args.width
+
+    if infile.startswith('http://') or infile.startswith('https://'):
+        infile, _ = urllib.request.urlretrieve(infile)
     else:
-        path = sys.argv[1]
-    runner(path)
+        if not os.path.isfile(infile):
+            print('Infile does not exist')
+            exit(1)
+
+    convert(infile, outfile, width)
